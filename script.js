a93b5aa4823c2858515ca18a7ca00b8750885064
Saurinnn <44335507+Saurinnn@users.noreply.github.com>
2026-03-05 13:25:52 +0900

    トップページJS実装完了

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const targets = [
+    ".mycover-text01", ".mycover-text02", ".inquire-cover",
+    ".section-title",
+    ".about-textbox",
+    ".about02-content-left", ".about02-content-right",
+    ".game-content", ".service-content",
+    ".ceo-h3", ".ceo", ".ceo-introduce", ".office-info-left", ".office-info-right", ".map", ".office-photo",
+    ".link-list",
+    ".contact-h2", ".contact-title-p", ".contact-button", ".contact-p2", ".contact-detail"
+  ];
+
+  const elements = document.querySelectorAll(targets.join(', '));
+
+  if (elements.length > 0) {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("fade-in");
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+
+    elements.forEach(element => observer.observe(element));
+  }
+});
